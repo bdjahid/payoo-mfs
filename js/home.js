2ca7d4d7435f5b2eabad54630f70cee511e951c4
Jahidul Islam <bdjahid363@gmail.com>
2025-04-20 06:45:56 +0600

    add balance

---
+++ js/home.js
@@ -0,0 +1,34 @@
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("btn add money");
+
+    const inputAddAmount = document.getElementById("input-add-amount").value;
+    console.log(inputAddAmount);
+
+    const inputPin = document.getElementById("input-pin").value;
+    console.log(inputPin);
+
+    if (inputPin === "1234") {
+      console.log("your amount is added");
+      // step2
+      const balance = document.getElementById("balance").innerText;
+      //   console.log(balance);
+
+      //   const newBalance1 = balance + inputAddAmount;
+      //   console.log(newBalance1);
+      // step3
+      const addMoneyNumber = parseFloat(balance);
+      // step4
+      const balanceNumber = parseFloat(inputAddAmount);
+      //   console.log(addMoneyNumber);
+      // step5
+      const newBalance = addMoneyNumber + balanceNumber;
+      console.log(newBalance);
+      // step6
+      document.getElementById("balance").innerText = newBalance;
+    } else {
+      alert("failed your add amount please valid pin");
+    }
+  });
--- js/login.js
+++ js/login.js
@@ -2,5 +2,17 @@ document
   .getElementById("btn-login")
   .addEventListener("click", function (event) {
     event.preventDefault();
+    const phoneNumber = document.getElementById("phone-number").value;
+    console.log(phoneNumber);
+
+    const pin = document.getElementById("pin").value;
+    console.log(pin);
+
+    if (phoneNumber === "5" && pin === "1234") {
+      console.log("you r logged  in");
+      window.location.href = "/home.html";
+    } else {
+      alert("Wrong phone or pin");
+    }
     console.log("click");
   });
